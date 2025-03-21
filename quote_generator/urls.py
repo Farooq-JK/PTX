7f6b35f6562ef1cd3f273a85300ae4dfcf71f79e
Farooq-JK <sqlpygenius@gmail.com>
2025-03-21 20:24:19 +0000

    Add files via upload

---
+++ quote_generator/urls.py
@@ -0,0 +1,8 @@
+# quote_generator/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('quotes.urls')),
+]
